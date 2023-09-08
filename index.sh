0d793f811e3a45c34505c4c28af9b9fa7e29b5c9
humfla123 <88781888+humfla123@users.noreply.github.com>
2023-09-08 19:18:22 -0300

    Update index.sh

--- index.sh
+++ index.sh
@@ -1,15 +1,11 @@
 apt update -y
 apt upgrade -y
-apt install openssh-server curl sudo wget -y
-rm -r /etc/ssh/sshd_config
-wget https://github.com/humfla123/deepnote/raw/main/sshd_config
-cp sshd_config /etc/ssh/sshd_config
-rm -r sshd_config
-service ssh restart
+apt install xfce4 xrdp curl sudo wget -y
+service xrdp restart
 curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list && sudo apt update && sudo apt install ngrok
 ngrok config add-authtoken 25zOFhO9xHS6LJflCnRDtgrWFGl_23YL1E4RHatGeLtdXGh3M
 echo "root:040112" | sudo chpasswd
-service ssh restart
+service xrdp restart
 echo Analizando finais... PRONTO!
 sleep 10
-ngrok tcp 22
+ngrok tcp 3389 --region sa
